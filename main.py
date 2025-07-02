f7709546cc2ede505d22667d19a1fb1f6d4ced10
onyi4677 <onyi4677@gmail.com>
2025-07-02 20:08:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask
+from alerts import check_market
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Crypto Alert is Live"
+
+@app.route("/check-now")
+def check_now():
+    check_market()
+    return "Manual market check triggered!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
